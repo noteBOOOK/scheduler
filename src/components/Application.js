acd7308ea02311c09f2417d05b1ceb5ad6b8d642
noteBOOOK <jason.long77@hotmail.com>
2020-10-28 19:18:48 -0400

    removed hardcoded appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,53 +6,6 @@ import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time:"2pm",
-//     interview: {
-//       student: "Alexander Scaini",
-//       interviewer: { id: 2,
-//         name: "Tori Malcolm",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png" 
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//     interview: {
-//       student: "Jason Long",
-//       interviewer: { 
-//         id: 5,
-//         name: "Sven Jones",
-//         avatar: "https://i.imgur.com/twYrpay.jpg"
-//       }
-//     }
-//   }
-// ];
-
 export default function Application(props) {
 
   const setDay = day => setState({ ...state, day});
