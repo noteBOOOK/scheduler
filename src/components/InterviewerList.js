8146c846d687ea5179a637087590c7fa486701f4
noteBOOOK <jason.long77@hotmail.com>
2020-11-02 13:36:06 -0500

    Implemented propTypes in interviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,14 @@
 import React from "react";
-
 import InterviewerListItem from "./InterviewerListItem";
-
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
 
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
+  
   const parsedInterviewers = props.interviewers.map(interviewer => {
     return <InterviewerListItem 
       key={interviewer.id}
